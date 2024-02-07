95b1609d778933659da846eaadc7088c121fa035
scarfacedotcom <peteradaaku@gmail.com>
2024-02-07 05:44:42 +0100

    decoing JSON

--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"golang-blockchain/utils"
 	"golang-blockchain/wallet"
 	"io"
@@ -76,7 +78,26 @@ func (ws *WalletServer) Wallet(w http.ResponseWriter, req *http.Request) {
 func (ws *WalletServer) CreateTransaction(w http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodPost:
-		io.WriteString(w, string(utils.JsonStatus("success")))
+		decoder := json.NewDecoder(req.Body)
+		var t wallet.TransactionRequest
+		err := decoder.Decode(&t)
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+			io.WriteString(w, string(utils.JsonStatus("FAIL")))
+			return
+		}
+		if !t.Validate() {
+			log.Println("ERROR: MISSING FIELDS")
+			io.WriteString(w, string(utils.JsonStatus("FAIL")))
+			return
+		}
+
+		fmt.Println(*t.SenderPublicKey)
+		fmt.Println(*t.SenderBlockchainAddress)
+		fmt.Println(*t.SenderPrivateKey)
+		fmt.Println(*t.RecipeintBlockchainAddress)
+		fmt.Println(*t.Value)
+
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 		log.Println("ERROR: Invalid HTTP Request")
